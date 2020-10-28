5c963e67d1d5743068c810149a4353928cabd117
cout <hamidkeyhani8761@gmail.com>
2020-10-28 12:39:04 +0330

    set on click in contact items

--- src/components/contact/Contact.vue
+++ src/components/contact/Contact.vue
@@ -2,7 +2,33 @@
 <style scoped src="./Contact.css"></style>
 <script>
 export default {
-name: "Contact"
+name: "Contact",
+  data() {
+    return {
+      socialLinks: [
+          "+989034017610",
+          "hamidkeyhani8761@gmail.com",
+          "Iran - Semnan Province - Semnan",
+          "https://stackoverflow.com/users/8399393/hamid-keyhani",
+          "https://github.com/hamidkeyhani",
+          "https://gitlab.com/cout",
+          "https://join.skype.com/invite/bid8xHmVl54T",
+          "https://telegram.me/coutt",
+          "https://www.instagram.com/hamidkeyhani_/"
+      ]
+    }
+  },
+  methods: {
+    onSocialClick(url) {
+      window.open(url)
+    },
+    sendEmail() {
+      window.location = "mailto:hamidkeyhani8761@gmail.com"
+    },
+    callToMe() {
+      document.location.href = "tel:+989034017610"
+    }
+  }
 }
 </script>
 
