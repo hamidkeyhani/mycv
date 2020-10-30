59490acc7692bcfc8f03d4545d55b084f99129f3
cout <hamidkeyhani8761@gmail.com>
2020-10-30 11:33:30 +0330

    works added

--- src/components/works/Works.vue
+++ src/components/works/Works.vue
@@ -2,6 +2,49 @@
 <style scoped src="./Works.css"></style>
 <script>
 export default {
-name: "Accomplishments"
+name: "Accomplishments",
+  data() {
+    return {
+      works: [
+        {
+          company: "Iranian Credit Card",
+          appLink: "",
+          pic: "https://devv.ir/screen/Iccard/iccard1.jpg",
+          title: "",
+          screenShots: "https://devv.ir/screen/Iccard",
+          description: "I work as android developer on this project"
+        },
+        {
+          company: "Fava",
+          appLink: "https://cafebazaar.ir/app/ir.semnan.app.tourism",
+          pic: "https://devv.ir/screen/SemnanTourism/semnan2.jpg",
+          title: "Semnan Tourism App",
+          screenShots: "https://devv.ir/screen/SemnanTourism",
+          description: "I work as android developer on this project"
+        },
+        {
+          company: "Private",
+          appLink: "",
+          pic: "https://devv.ir/screen/Iccard/iccard5.jpg",
+          title: "",
+          screenShots: "https://devv.ir/screen/Iccard",
+          description: "I work as android developer on this project"
+        },
+        {
+          company: "Asa Technology Group",
+          appLink: "",
+          pic: "https://devv.ir/screen/Iccard/iccard3.jpg",
+          title: "",
+          screenShots: "https://devv.ir/screen/Iccard",
+          description: "I work as android developer on this project"
+        }
+      ]
+    }
+  },
+  methods: {
+    openNewTab(url) {
+      window.open(url)
+    }
+  }
 }
 </script>
