41f61ba32f9851ebdd264fbcb42e9f622fb28f6c
cout <hamidkeyhani8761@gmail.com>
2020-11-03 07:33:28 +0330

    add works title & technologies

--- src/components/works/Works.vue
+++ src/components/works/Works.vue
@@ -11,9 +11,19 @@ name: "Accomplishments",
           appLink: "https://play.google.com/store/apps/details?id=ir.iccard.app&hl=en_US",
           pic: "https://devv.ir/screen/Iccard/iccard2.jpg",
           screenShots: "https://devv.ir/screen/Iccard",
-          description: "I work as android developer on this project",
+          description: "free and user-friendly mobile payment application that provides highly secure access to a wide range of payment services.",
           video: "",
-          tech: "",
+          tech: "MVVM Architecture," +
+              " Kotlin coroutines," +
+              " Dagger," +
+              " RxJava & RxAndroid," +
+              " Retrofit," +
+              " Gson," +
+              " Data Binding," +
+              " Socket.io," +
+              " Google Map," +
+              " Shimmer Library, ..."
+          ,
           preLink: "Download"
         },
         {
@@ -21,7 +31,7 @@ name: "Accomplishments",
           appLink: "https://cafebazaar.ir/app/ir.semnan.app.tourism",
           pic: "https://devv.ir/screen/SemnanTourism/semnan2.jpg",
           screenShots: "https://devv.ir/screen/SemnanTourism",
-          description: "This is a Tourism Application for Semnan Province. I develop this app as senior android developer in Semnan Information and Communication Technology (Fava)",
+          description: "A suitable companion for traveling to Semnan",
           video: "",
           tech: "Recycler View, Navigation Menu, MVVM Architecture, Retrofit, RxJava, kotlin, data binding",
           preLink: "Download"
@@ -31,9 +41,16 @@ name: "Accomplishments",
           appLink: "https://play.google.com/store/apps/details?id=ir.iccard.app&hl=en_US",
           pic: "https://devv.ir/screen/GroceryApp/Grocery2.jpg",
           screenShots: "https://devv.ir/screen/GroceryApp",
-          description: "This is a Grocery Android Application for Semnan Province",
+          description: "This is a Online Grocery Android Application for Semnan Province",
           video: "",
-          tech: "",
+          tech: " MVVM architecture," +
+              " RxJava & RxAndroid," +
+              " Kotlin," +
+              " Room," +
+              " Retrofit,"+
+              " Android Animations," +
+              " Android Material Design Library, ..."
+          ,
           preLink: "Download"
         },
         {
@@ -43,7 +60,11 @@ name: "Accomplishments",
           screenShots: "https://devv.ir/screen/SmartHome",
           description: "At Asa Technology Group I Develop Smart Home Android Application for control our products such as smart key & smart strip",
           video: "",
-          tech: "",
+          tech: " MVVM architecture," +
+              " RxJava & RxAndroid," +
+              " Kotlin," +
+              " Socket (UDP & TCP)," +
+              " Bluetooth API, ...",
           preLink: "Download"
         },
         {
@@ -53,7 +74,7 @@ name: "Accomplishments",
           screenShots: "",
           description: "This is a progress bar library for android",
           video: "",
-          tech: "",
+          tech: "Kotlin, Java, Android Animations, Android Library",
           preLink: "Github"
         }
       ]
